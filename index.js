61d4cd17dc8eeedf605e744349fcc695ab41e13f
Your Name <kanukaharish@gmail.com>
2024-02-20 20:00:14 +0530

    installed ejs to update the data dynamically

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 import express from "express";
 import ProductController from "./src/controllers/product.controller.js";
-// const express = require('express');
+import path from "path";
 
 const server = express();
-
+//Setup view engine settings
+server.set("view engine", "ejs");
+server.set("views", path.join(path.resolve(), "src", "views"));
 // create an instance of ProductController
 const productController = new ProductController();
 server.get("/", productController.getProducts);
 server.use(express.static("src/views"));
-// return res.send('Welcome to Inventory App');
 server.listen(3400);
 console.log("Server is listening on port 3400");
--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -5,8 +5,9 @@ export default class ProductController {
     getProducts(req, res) {
         let products = ProductModel.get();
         console.log(products);
-        return res.sendFile(
-            path.join(path.resolve(), "src", "views", "products.html")
-        );
+        res.render("products", { products: products });
+        // return res.sendFile(
+        //     path.join(path.resolve(), "src", "views", "products.html")
+        // );
     }
 }
