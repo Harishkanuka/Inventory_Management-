7bfbe60a47e078f6aad4f85924d489299734f032
Your Name <kanukaharish@gmail.com>
2024-02-21 08:46:29 +0530

    Created form for the new product

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 import express from "express";
 import ProductController from "./src/controllers/product.controller.js";
 import path from "path";
+import ejsLayouts from "express-ejs-layouts";
 
 const server = express();
 //Setup view engine settings
 server.set("view engine", "ejs");
 server.set("views", path.join(path.resolve(), "src", "views"));
+
+//setup ejs layout
+server.use(ejsLayouts);
 // create an instance of ProductController
 const productController = new ProductController();
 server.get("/", productController.getProducts);
+server.get("/new", productController.getAddForm);
+server.post("/", productController.getProducts);
 server.use(express.static("src/views"));
 server.listen(3400);
 console.log("Server is listening on port 3400");
--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -4,10 +4,18 @@ import ProductModel from "../models/product.model.js";
 export default class ProductController {
     getProducts(req, res) {
         let products = ProductModel.get();
-        console.log(products);
+        // console.log(products);
         res.render("products", { products: products });
         // return res.sendFile(
         //     path.join(path.resolve(), "src", "views", "products.html")
         // );
+    }
+    getAddForm(req, res) {
+        return res.render("new-product");
+    }
+    addNewProduct(req, res) {
+        console.log(res.body);
+        let products = ProductModel.get();
+        res.render("products", { products: products });
     }
 }
